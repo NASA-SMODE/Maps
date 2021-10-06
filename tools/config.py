a78469b1178620e183ff429f5e52bce25de8127b
Cesar B. Rocha <rocha.cesarb@protonmail.com>
2021-10-06 12:01:16 -0400

    Add generic SMODE tools and configurations

---
+++ tools/config.py
@@ -0,0 +1,10 @@
+# S-MODE Operations Area polygon
+# see experimental plan at https://docs.google.com/document/d/15TvXY8myX7MdzyIUlb4i7SU77kgnwSf_/edit?pli=1
+OPSAREA = {'longitude': [-125.3678, -124.0011, -123.1667,
+			 -122.7178, -124.3178, -125.3678],
+           'latitude': [38.0917, 37.7167, 37.7736,
+                        37.0833, 36.3333, 38.0917]       
+          }
+
+# Map extent
+MAPEXTENT = [-126.,-121.,36.,39.]
