3e4bcc2125fef71e96f7f58337279f5e8e373778
cesar-rocha <rocha.cesarb@protonmail.com>
2021-10-13 19:09:58 -0400

    minor update to polygon area

--- tools/config.py
+++ tools/config.py
@@ -1,9 +1,9 @@
 # S-MODE Operations Area polygon
 # see experimental plan at https://docs.google.com/document/d/15TvXY8myX7MdzyIUlb4i7SU77kgnwSf_/edit?pli=1
-OPSAREA = {'longitude': [-125.36778333, -124.00111667, -123.43333333, 
-                         -122.95, -124.31778333, -125.36778333],
-           'latitude': [38.00833333, 37.71666667, 37.75, 
-                        36.96666667, 36.33333333, 38.00833333]
+OPSAREA = {'longitude': [-125.36778333, -124.00111667, -123.43673333, 
+                         -122.95146667, -124.31778333, -125.36778333],
+           'latitude': [38.09166667, 37.71666667, 37.75518333, 36.97378333, 
+                        36.33333333, 38.09166667]
           }
 
 # Map extent
